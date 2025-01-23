e0794e125b02b40b0511a8afbe2d169dd13423d1
NunoF <n.fernandes.contact@gmail.com>
2025-01-23 17:35:52 +0100

    create JobApplication model and serializer

--- backend/api/models.py
+++ backend/api/models.py
@@ -8,5 +8,9 @@ class JobApplication(models.Model):
     company_email = models.EmailField(max_length=100)
     job_link = models.CharField(max_length=100)
     status = models.CharField(max_length=100)
+    comments = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.job + ' at ' + self.company_name}"
